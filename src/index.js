6eee487759f39d9abf5779e37755c089c282ffad
i3-code <i3pobox@gmail.com>
2020-09-10 17:33:26 +0300

    feat: add Brackets solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  return 0;
+  let found = 1;
+  let oldStr;
+  while(found == 1) {
+    oldStr = str;
+    for (let i of bracketsConfig) {
+      str = str.split(i[0] + i[1]).join('');
+    }
+    if (oldStr === str) { found = 0; }
+}
+  return (str.length == 0) ? true : false;
 }
